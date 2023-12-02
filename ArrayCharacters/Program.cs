8b427ff018ae92675d9c9f510d911d7e7b617ab6
antoni1913 <antonisariev13@gmail.com>
2023-12-03 00:58:32 +0200

    week6A

---
+++ ArrayCharacters/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] fruitBasket = { "apples", "bananas", "oranges" };
+
+Console.WriteLine($"there are {fruitBasket.Length} fruit in your basket ");
+
+
+
+
+fruitBasket[0] = "pine-apple";
+fruitBasket[2] = "grape";
+
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+
+}
+
+
+
+string[] colors = { "red", "green", "blue", "pink", "yellow" };
+Random rnd = new Random();
+string randomColor = colors[rnd.Next(0, colors.Length)];
+Console.WriteLine($"You should wear {randomColor} today.");
---
+++ Week6Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿string userName = "antoni Darth Vader";
+
+Console.WriteLine(userName.Length);
+
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}.");
+
+string updaterUserName = userName.Replace('a', '4');
+
+Console.WriteLine(updaterUserName);
+
+string hello = "Hello, world!".
+ToLower
+();
+int counter = 0;
+for (int i = 0; i <
+hello.Length
+; i++)
+{
+    if (
+hello
+[i] == 'l')
+    {
+
+        counter
+        ++;
+    }
+}
+Console.WriteLine($"There are {counter} l's in the string \"{hello}\"");
+//
